e58ce73337da30e5d092ed423b6aebff88fc71bb
Daniel Lynch <daniel.lynch@paul-mccoy.com>
2020-04-15 12:11:04 -0400

    Fixed bug which threw errors to console when interacting without items in hand

--- src/ch/dlyn/hoverpack/InteractListener.java
+++ src/ch/dlyn/hoverpack/InteractListener.java
@@ -9,7 +9,7 @@ import org.bukkit.inventory.ItemStack;
 public class InteractListener implements Listener {
 	@EventHandler
 	public void onInteract(PlayerInteractEvent e) {
-		if (e.getItem().getType().equals(HoverPack.getRefinedFuel().getType())) {
+		if (e.getItem() != null && e.getItem().getType().equals(HoverPack.getRefinedFuel().getType())) {
 			if (e.getItem().getEnchantments().containsKey(Enchantment.ARROW_INFINITE)) {
 				for (ItemStack item : e.getPlayer().getInventory().all(HoverPack.getNewHoverPack().getType()).values()) {
 					if (item.getEnchantments().containsKey(Enchantment.ARROW_INFINITE)) {
